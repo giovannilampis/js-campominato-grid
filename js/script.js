7ad7d840338ea5788758da7bedaf74ee819610d5
giovannilampis <giolampis@hotmail.com>
2023-03-31 16:03:31 +0200

    function creates generic element with some classes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 "use strict"
+
+// a function can create a generic html element with some certain classes
+
+function createHtmlElement(htmlTag, classes){
+    let element = document.createElement( htmlTag );
+    element.className = classes;
+    return element;
+}
+
+console.log( createHtmlElement( "div", "blue-cell" ) )
