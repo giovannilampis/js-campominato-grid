bc56357c5949e023ca13b72882bc6fa13eddf4f2
giovannilampis <giolampis@hotmail.com>
2023-04-01 06:07:23 +0200

    add comments to project

--- js/script.js
+++ js/script.js
@@ -41,28 +41,44 @@ function createHtmlElement(htmlTag, classes, text){
 
     const level = document.getElementById("level").value;
 
+
+    // there will be differences depending on the difficulty level chosen
+
     switch(level) {
+
+        // level beginner
 
         case '100':
         default:
            element.classList.add("beginner-cell");
            break;
 
+        // level advanced
+
         case '81':
              element.classList.add("advanced-cell");
              break;
+
+        // level pro
 
          case '49':
              element.classList.add("pro-cell");
      }
 
+    //  each item will be clickable
+
      element.addEventListener("click", function() {
+
+        // when clicked, the element will turn azure
+
         this.classList.add("azure");
+
+        // when clicked, the element will show a text
+
         this.innerHTML = text;
+
      })
 
     return element;
 
 }
-
-// console.log( createHtmlElement( "div", "blue-cell" ) );
